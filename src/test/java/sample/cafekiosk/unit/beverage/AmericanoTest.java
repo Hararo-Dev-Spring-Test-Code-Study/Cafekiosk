5f7a696b9580d90eeff0bf279283eb72682872b9
KimGiii <gibo9809@gmail.com>
2025-05-20 23:40:20 +0900

    Test: Americano의 이름과 가격을 확인하는 테스트

--- src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
+++ src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.unit.beverage;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,12 +8,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AmericanoTest {
 
+    @DisplayName("아메리카노의 이름 확인")
     @Test
     void getName() {
+        Americano americano = new Americano();
+
+        assertThat(americano.getName()).isEqualTo("아메리카노");
     }
 
+    @DisplayName("아메리카노의 가격을 확인")
     @Test
     void getPrice() {
+        Americano americano = new Americano();
+
+        assertThat(americano.getPrice()).isEqualTo(4000);
     }
 
 }
