06649a1bc591457ff5f2f3058161d7043e20ad88
Gibo Kim <gibo9809@gmail.com>
2025-05-26 22:49:47 +0900

    미션5 공지

---
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -0,0 +1,12 @@
+package sample.cafekiosk.spring.domain.product;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// @SpringBootTest
+@DataJpaTest
+class ProductRepositoryTest {
+    
+}
