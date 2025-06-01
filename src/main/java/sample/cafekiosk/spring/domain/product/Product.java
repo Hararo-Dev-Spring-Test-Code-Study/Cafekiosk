fd7018d5d8d02195183f09cbc9c17468288a55d7
VarGun <heegun46900@gmail.com>
2025-06-02 03:56:09 +0900

    ProductRepositoryTest 작성 및 Product.java 수정

--- src/main/java/sample/cafekiosk/spring/domain/product/Product.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -7,16 +7,17 @@ import sample.cafekiosk.spring.domain.BaseEntity;
 
 import javax.persistence.*;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity
+@Getter // 클래스 내의 모든 필드들에 getter 메서드 생성
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자를 만들어줌. JPA 에서는 파라미터가 없는 기본 생성자를 만드는 것이 필수적이라고 함.
+@Entity // JPA 엔티티 클래스임. JPA 가 이 클래스를 DB 테이블로 매핑
 public class Product extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // 이 속성이 기본 키임.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 이 키 값을 자동으로 생성. auto increment 방식
     private Long id;
 
     private String productNumber;
 
+    // String 타입으로 EnumType 을 저장하겠다는 의미 / ORDINAL : 순서 값을 저장하겠음 ex) HANDMADE : 0, BOTTLE : 1 ..
     @Enumerated(EnumType.STRING)
     private ProductType type;
 
@@ -26,5 +27,15 @@ public class Product extends BaseEntity {
     private String name;
 
     private int price;
+
+    // 추가도 테스트 해보고 싶어서 생성자 추가
+    // @Setter 를 통해서 테스트 할 수도 있음
+    public Product(String productNumber, ProductType type, ProductSellingStatus status, String name, int price) {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = status;
+        this.name = name;
+        this.price = price;
+    }
 
 }
--- src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -2,23 +2,60 @@ package sample.cafekiosk.spring.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
-// @SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@SpringBootTest
 @DataJpaTest
 class ProductRepositoryTest {
+    @Autowired //
+    private ProductRepository productRepository;
 
-    @DisplayName("")
+    @DisplayName("판매중 또는 판매보류 상태의 상품만 조회된다")
     @Test
     void findAllBySellingStatusIn() {
-        // given
 
-        // when
+        // when - forDisplay() 를 통해 SELLING, HOLD 만 필터링
+        // findAllBySellingStatusIn 의 sellingTypes 에 [SELLING, HOLD] 전달
+        // data.sql 을 통해 타입이 selling_status 가 SELLING 과 HOLD 인 데이터가 하나씩 들어가 있음.
+        List<Product> result = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+        List<Product> gun = productRepository.findAll();
+        for(Product p : gun){
+            System.out.println(p.getCreatedDateTime());
+        }
 
         // then
-
+        // 2 개인지 확인하고,
+        // sellingStatus 를 추출해서 SELLING 이고, HOLD 인지 확인
+        assertThat(result).hasSize(2)
+            .extracting("sellingStatus")
+            .containsExactlyInAnyOrder(ProductSellingStatus.SELLING, ProductSellingStatus.HOLD);
     }
+
+//    @DisplayName("판매중 또는 판매보류 상태의 상품을 저장하고 조회하면, 해당 상품만 반환된다")
+//    @Test
+//    void saveAndFindProductsBySellingStatus() {
+//        // 데이터를 추가하는 등의 로직을 검증하려면 아래 deleteAll 을 통해 초기화 후 사용
+//         productRepository.deleteAll();
+//        // given
+//        Product product1 = new Product("001", ProductType.HANDMADE, ProductSellingStatus.SELLING, "아메리카노", 4000);
+//        Product product2 = new Product("002", ProductType.HANDMADE, ProductSellingStatus.HOLD, "카페라떼", 4500);
+//        Product product3 = new Product("003", ProductType.BAKERY, ProductSellingStatus.STOP_SELLING, "크루아상", 3500);
+//
+//        productRepository.saveAll(List.of(product1, product2, product3));
+//
+//        // when
+//        List<Product> result = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+//
+//        // then
+//        assertThat(result).hasSize(2)
+//            .extracting("sellingStatus")
+//            .containsExactlyInAnyOrder(ProductSellingStatus.SELLING, ProductSellingStatus.HOLD);
+//    }
+
 }
