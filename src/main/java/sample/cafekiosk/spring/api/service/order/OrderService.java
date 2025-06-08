511d739b601486b52c3ca5cb61760b48b69f5165
KimGiii <gibo9809@gmail.com>
2025-06-08 22:42:56 +0900

    Mission6: orderController 생성

---
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -0,0 +1,13 @@
+package sample.cafekiosk.spring.api.service.order;
+
+import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+
+@Service
+public class OrderService {
+    // 주문 생성에 대한 비즈니스 로직
+    public void createOrder(OrderCreateRequest request) {
+
+
+    }
+}
