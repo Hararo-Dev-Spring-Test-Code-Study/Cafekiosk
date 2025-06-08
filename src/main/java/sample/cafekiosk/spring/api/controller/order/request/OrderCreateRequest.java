ff08ee93c6f5efb38872e054cef14751c957ee16
jaehee <wogml414@gmail.com>
2025-06-09 06:37:30 +0900

    [Week6] OrderControllerTest를 위한 OrderController 구현, OrderCreateRequest.java에 기본 생성자 생성해주도록 수정

--- src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
@@ -1,11 +1,14 @@
 package sample.cafekiosk.spring.api.controller.order.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor // 기본 생성자 생성
 public class OrderCreateRequest {
 
     private List<String> productNumbers;
