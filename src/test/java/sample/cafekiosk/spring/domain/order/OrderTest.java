78205a7e77f8b0b8336225f14552a586138292f1
박승희 <ridia05031@gmail.com>
2025-06-09 01:56:34 +0900

    [Week 6] 주문의 총 금액을 계산할 수 있어야 한다 요구사항의 Red 단계

--- src/test/java/sample/cafekiosk/spring/domain/order/OrderTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/order/OrderTest.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.domain.order;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductType;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -26,5 +28,32 @@ class OrderTest {
         assertThat(order.getProductNumbers()).hasSize(3)
                 .containsExactly("001", "002", "003");
         assertThat(order.getRegisteredDateTime()).isEqualTo(registeredDateTime);
+    }
+
+    @Test
+    @DisplayName("주문의 총 금액을 계산할 수 있다")
+    void calculateTotalAmount() {
+        // given
+        List<Product> products = List.of(
+                createProduct("001", 1000),
+                createProduct("002", 2000),
+                createProduct("003", 3000)
+        );
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        // when
+        Order order = Order.create(products, registeredDateTime);
+
+        // then
+        assertThat(order.getTotalAmount()).isEqualTo(6000);
+    }
+
+    private Product createProduct(String productNumber, int price) {
+        return Product.builder()
+                .productNumber(productNumber)
+                .type(ProductType.HANDMADE)
+                .price(price)
+                .name("메뉴 이름")
+                .build();
     }
 } 
