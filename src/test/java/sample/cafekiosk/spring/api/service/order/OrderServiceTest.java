65584badb9c73304d340d5d13b2b4f94300b7f17
KimGiii <gibo9809@gmail.com>
2025-06-15 14:12:00 +0900

    Mission7: 재고 차감시 예외 발생 테스트

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -1,10 +1,8 @@
 package sample.cafekiosk.spring.api.service.order;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
@@ -22,9 +20,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.tuple;
-import static org.junit.jupiter.api.Assertions.*;
-import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
 import static sample.cafekiosk.spring.domain.product.ProductType.*;
 
 // 여기서 dataJpaTest를 진행하면 orderService를 못찾아서 테스트 오류 발생
@@ -50,13 +48,13 @@ class OrderServiceTest {
 
 //    @AfterEach
 //    void tearDown() {
-////        productRepository.deleteAll();
+
+    /// /        productRepository.deleteAll();
 //        orderProductRepository.deleteAllInBatch();
 //        productRepository.deleteAllInBatch();
 //        orderRepository.deleteAllInBatch();
 //        stockRepository.deleteAllInBatch();
 //    }
-
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     @Test
     void createOrder() {
@@ -143,7 +141,7 @@ class OrderServiceTest {
         stockRepository.saveAll(List.of(stock1, stock2));
 
         OrderCreateRequest request = OrderCreateRequest.builder()
-                .productNumbers(List.of("001","001", "002", "003"))
+                .productNumbers(List.of("001", "001", "002", "003"))
                 .build();
 
         // when
@@ -174,9 +172,35 @@ class OrderServiceTest {
                 );
     }
 
+    @DisplayName("재고가 부족한 상품으로 주문을 생성하려는 경우 주문할 수 없다.")
+    @Test
+    void createOrderWithNoStock() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 4000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 1);
+        Stock stock2 = Stock.create("002", 1);
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        // when // then
+        assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("재고가 부족한 상품이 있습니다.");
+
+    }
+
     // builder 패턴을 사용하여 given에 주어진 정보가 너무 길어 -> 테스트에 필요한 정보와 필요하지 않은 정보를 구분해서
     // 테스트를 깔끔하게 문서 형태로 작성하기 위해 Product를 만드는 도우미 메서드 생성
-    private Product createProduct(ProductType type, String ProductNumber, int price ) {
+    private Product createProduct(ProductType type, String ProductNumber, int price) {
         return Product.builder()
                 .type(type)
                 .productNumber(ProductNumber)
