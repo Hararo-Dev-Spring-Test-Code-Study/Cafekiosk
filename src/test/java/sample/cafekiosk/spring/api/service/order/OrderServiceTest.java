783169fe4ffa0fae9bb1f48200b54bded3c216e8
jaehee <wogml414@gmail.com>
2025-06-16 03:33:38 +0900

    [Week 7] OrderServiceTest 테스트코드 작성

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -2,11 +2,24 @@ package sample.cafekiosk.spring.api.service.order;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+// 스프링이 관리하는 Bean을 자동으로 주입해주는 어노테이션
+// OrderService 타입의 Bean을 자동으로 찾아 orderService 필드에 자동으로 주입해준다
 import org.springframework.beans.factory.annotation.Autowired;
+
+// SpringBoot를 전체 기동해 통합 테스트 수행
 import org.springframework.boot.test.context.SpringBootTest;
+
+// test 프로필 설정 활성화 -> application.yml의 ---test 섹션 적용
 import org.springframework.test.context.ActiveProfiles;
+
+// 컨트롤러 패키지에 정의된 요청 DTO. 테스트에서 직접 생성해 전달
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+
+// 서비스 로직이 반환하는 응답 DTO
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+
+// 상품 entity(Product) 레포지토릴 및 관련 enum 사용
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductType;
@@ -19,28 +32,73 @@ import static org.assertj.core.api.Assertions.tuple;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
 import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
+// Week 7 추가된 package
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
+import sample.cafekiosk.spring.domain.orderproduct.OrderProductRepository;
+import sample.cafekiosk.spring.domain.stock.Stock;
+import sample.cafekiosk.spring.domain.stock.StockRepository;
+import static sample.cafekiosk.spring.domain.product.ProductType.*;
+import static org.assertj.core.api.Assertions.*;
+
+
+// 스프링이 실행될 때 test 프로필 적용 -> H2 DB와 data.sql 등 테스트 설정 사용
 @ActiveProfiles("test")
+
+// 애플리케이션 전체 컨텍스트 기동, 실제 Bean 주입 -> 통합 테스트 환경
 @SpringBootTest
 //@DataJpaTest
 class OrderServiceTest {
 
+    // Autowired : 실제 구현체(Spring Bean)이 주입됨
+    // ProductRepository : 상품 데이터를 DB에 저장/조회
+    // OrderService : 주문 비즈니스 로직 핵심 클래스
     @Autowired
     private ProductRepository productRepository;
 
     @Autowired
     private OrderService orderService;
 
+    // Week 7 추가
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderProductRepository orderProductRepository;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    // 테스트시 테스트 데이터 정리(clean up)을 위해 사용되는 메서드
+    // 각 테스트 메서드 실행 후 자동으로 실행되는 메서드에 붙임
+    // 주로 리소스 정리, 테스트 데이터 삭제 용도로 사용
+    @AfterEach
+    void tearDown() {
+        // deleteAllInBatch() : 한번의 SQL로 select 없이 바로 전체 삭제 처리
+        // deleteAll()은 개별 엔티티마다 select 후 delete 하므로 느림
+        // 테스트 간 독립성 보장을 위해 각 테스트 후 데이터 모두 삭제
+        orderProductRepository.deleteAllInBatch();
+        productRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+        stockRepository.deleteAllInBatch();
+    }
+
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     @Test
     void createOrder() {
         // given
+        // 현재시각을 주문 등록 시간으로 사용
         LocalDateTime registeredDateTime = LocalDateTime.now();
 
+        // 테스트용 상품 3개 생성하고 레포지토리에 실제 DB INSERT
         Product product1 = createProduct(HANDMADE, "001", 1000);
         Product product2 = createProduct(HANDMADE, "002", 3000);
         Product product3 = createProduct(HANDMADE, "003", 5000);
         productRepository.saveAll(List.of(product1, product2, product3));
 
+        // 주문 생성 요청 DTO 작성
+        // 상품번호 001, 002 두개만 주문으로 생성
         OrderCreateRequest request = OrderCreateRequest.builder()
                 .productNumbers(List.of("001", "002"))
                 .build();
@@ -70,5 +128,111 @@ class OrderServiceTest {
                 .name("메뉴 이름")
                 .build();
     }
+
+    // Week 7 추가
+
+    @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
+    @Test
+    void createOrderWithDuplicateProductNumbers() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(HANDMADE, "001", 1000);
+        Product product2 = createProduct(HANDMADE, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001"))
+                .build();
+
+        // when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 2000);
+        assertThat(orderResponse.getProducts()).hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("001", 1000)
+                );
+    }
+
+    @DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트를 받아 주문을 생성한다.")
+    @Test
+    void createOrderWithStock() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 2);
+        Stock stock2 = Stock.create("002", 2);
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        // when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 10000);
+        assertThat(orderResponse.getProducts()).hasSize(4)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("001", 1000),
+                        tuple("002", 3000),
+                        tuple("003", 5000)
+                );
+
+        List<Stock> stocks = stockRepository.findAll();
+        assertThat(stocks).hasSize(2)
+                .extracting("productNumber", "quantity")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 0),
+                        tuple("002", 1)
+                );
+    }
+
+    @DisplayName("재고가 부족한 상품으로 주문을 생성하려는 경우 예외가 발생한다.")
+    @Test
+    void createOrderWithNoStock() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 2);
+        Stock stock2 = Stock.create("002", 2);
+        // "001" 상품 재고 -1
+        stock1.deductQuantity(1); // todo
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        // when // then
+        assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("재고가 부족한 상품이 있습니다.");
+    }
+
 
 }
