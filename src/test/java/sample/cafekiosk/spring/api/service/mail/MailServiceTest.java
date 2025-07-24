866213e5942766f86f419e852f02379452967d44
jinlee <dlwls6877@naver.com>
2025-07-24 17:43:03 +0900

    [Week11]- 순수 Mockito로 검증

--- src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -2,7 +2,6 @@ package sample.cafekiosk.spring.api.service.mail;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.*;
 import sample.cafekiosk.spring.domain.mail.Mail;
 import sample.cafekiosk.spring.domain.mail.MailRepository;
 import sample.cafekiosk.spring.domain.order.Order;
@@ -14,7 +13,8 @@ import sample.cafekiosk.spring.domain.product.ProductType;
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
+import static org.mockito.ArgumentMatchers.any;
 
 /**
  * MailService의 비즈니스 로직을 검증하는 단위 테스트 클래스입니다.
@@ -23,18 +23,15 @@ import static org.mockito.Mockito.*;
  */
 class MailServiceTest {
 
-    @Mock
     private OrderRepository orderRepository;
-
-    @Mock
     private MailRepository mailRepository;
-
-    @InjectMocks
     private MailService mailService;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
+        orderRepository = mock(OrderRepository.class);
+        mailRepository = mock(MailRepository.class);
+        mailService = new MailService(orderRepository, mailRepository);
     }
 
     /**
@@ -67,15 +64,15 @@ class MailServiceTest {
         Order order1 = new Order(List.of(product1), targetDate.atStartOfDay());
         Order order2 = new Order(List.of(product2), targetDate.atStartOfDay());
 
-        // 주문 레포지토리에서 해당 날짜의 결제완료 주문 목록을 반환하도록 설정
-        when(orderRepository.findCompletedOrdersByDate(targetDate))
-                .thenReturn(List.of(order1, order2));
+        // given
+        given(orderRepository.findCompletedOrdersByDate(targetDate))
+                .willReturn(List.of(order1, order2));
 
-        // when: 해당 날짜의 매출 요약 메일 전송 로직 실행
+        // when
         mailService.sendSalesSummaryMail(targetDate);
 
-        // then: 주문 조회와 메일 저장이 정확히 한 번씩 호출되었는지 검증
-        verify(orderRepository, times(1)).findCompletedOrdersByDate(targetDate);
-        verify(mailRepository, times(1)).save(any(Mail.class));
+        // then
+        then(orderRepository).should(times(1)).findCompletedOrdersByDate(targetDate);
+        then(mailRepository).should(times(1)).save(any(Mail.class));
     }
 }
